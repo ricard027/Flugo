2335015664e76015d181fde393d231066c64b0e3
Ricardo <ricardo_cabista@hotmail.com>
2025-06-24 16:52:31 -0300

    feat: more changes in profissionalInfoStep

--- app/_components/steps/ProfessionalInfoStep.tsx
+++ app/_components/steps/ProfessionalInfoStep.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import type React from "react"
-import { Grid, TextField, Typography, Box, MenuItem } from "@mui/material"
-import type { EmployeeData } from "../../types/employee"
+import type React from "react";
+import { TextField, Typography, Box, MenuItem } from "@mui/material";
+import type { EmployeeData } from "../../types/employee";
 
 interface ProfessionalInfoStepProps {
-  data: EmployeeData
-  errors: Record<string, string>
-  onChange: (data: Partial<EmployeeData>) => void
+  data: EmployeeData;
+  errors: Record<string, string>;
+  onChange: (data: Partial<EmployeeData>) => void;
 }
 
 const departments = [
@@ -19,12 +19,18 @@ const departments = [
   "Operações",
   "Jurídico",
   "Administrativo",
-]
+];
 
-export default function ProfessionalInfoStep({ data, errors, onChange }: ProfessionalInfoStepProps) {
-  const handleChange = (field: keyof EmployeeData) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChange({ [field]: event.target.value })
-  }
+export default function ProfessionalInfoStep({
+  data,
+  errors,
+  onChange,
+}: ProfessionalInfoStepProps) {
+  const handleChange =
+    (field: keyof EmployeeData) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      onChange({ [field]: event.target.value });
+    };
 
   return (
     <Box>
@@ -35,37 +41,36 @@ export default function ProfessionalInfoStep({ data, errors, onChange }: Profess
         Selecione o departamento do colaborador
       </Typography>
 
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            select
-            label="Selecione um departamento"
-            value={data.department}
-            onChange={handleChange("department")}
-            error={!!errors.department}
-            helperText={errors.department || "Escolha o departamento onde o colaborador irá trabalhar"}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                "&.Mui-focused fieldset": {
-                  borderColor: "#22c55e",
-                  borderWidth: 2,
-                },
-              },
-              "& .MuiInputLabel-root.Mui-focused": {
-                color: "#22c55e",
-              },
-            }}
-          >
-            {departments.map((dept) => (
-              <MenuItem key={dept} value={dept}>
-                {dept}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-      </Grid>
+      <TextField
+        required
+        fullWidth
+        select
+        label="Selecione um departamento"
+        value={data.department}
+        onChange={handleChange("department")}
+        error={!!errors.department}
+        helperText={
+          errors.department ||
+          "Escolha o departamento onde o colaborador irá trabalhar"
+        }
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused fieldset": {
+              borderColor: "#22c55e",
+              borderWidth: 2,
+            },
+          },
+          "& .MuiInputLabel-root.Mui-focused": {
+            color: "#22c55e",
+          },
+        }}
+      >
+        {departments.map((dept) => (
+          <MenuItem key={dept} value={dept}>
+            {dept}
+          </MenuItem>
+        ))}
+      </TextField>
     </Box>
-  )
+  );
 }
